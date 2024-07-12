8f266457f7ccd96965b38f1539c8f8de46319eef
Nyanchl <rotdicm@gmail.com>
2024-07-12 18:58:39 +0900

    任意のタイミングでURLを取得できる機能作成中

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,17 @@
-chrome.tabs.onActivated.addListener((activeInfo) => {
-    console.log("hello")
-    console.log(activeInfo.tabId)
-    chrome.tabs.get(activeInfo.tabId, (tab) => {
-        console.log(tab.url);
-    });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "getLastAccessedUrl") {
+        chrome.tabs.onActivated.addListener((activeInfo) => {
+            console.log("hello")
+            console.log(activeInfo.tabId)
+            chrome.tabs.get(activeInfo.tabId, (tab) => {
+                if (tab.url) {
+                    sendResponse({ url: tab.url })
+                } else {
+                    sendResponse({ url: "" })
+                }
+            });
+            return true;
+        });
+    }
 });
--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,30 @@ import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("")
+  // function getCurrentTab() {
+  //   const queryOptions = { active: true, lastFocusedWindow: true };
+  //   // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  //   const tab = chrome.tabs.query(queryOptions);
+  //   console.log(tab);
+  // }
   console.log("active get")
   document.addEventListener('DOMContentLoaded', () => {
     console.log('Popup loaded');
   });
-  // chrome.tabs.onActivated.addListener((activeInfo) => {
-  //   console.log("hello")
-  //   console.log(activeInfo.tabId)
-  //   chrome.tabs.get(activeInfo.tabId, (tab) => {
-  //     console.log(tab.url);
-  //   });
-  // });
+  const getUrl = () => {
+    chrome.runtime.sendMessage({ action: "getLastAccessedUrl"}, response => {
+      if (response.url) {
+        setUrl(response.url)
+      } else {
+        setUrl("No URL found")
+      }
+    })
+  }
   return (
     <>
       <button onClick={() => setCount(count + 1)}>count is: {count}</button>
+      <button onClick={getUrl}>Get URL{url}</button>
     </>
   )
 }
