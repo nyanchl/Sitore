b8a5b5e89c1892edae3cbae4b83599692bf730c0
Nyanchl <rotdicm@gmail.com>
2024-07-17 10:07:39 +0900

    Notionのdbにデータを追加する処理作成中

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "getLastAccessedUrl") {
@@ -10,5 +11,21 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             }
         });
         return true;
+    }
+
+    if (message.action === "addInNotion") {
+        const token = import.meta.env.VITE_NOTION_TOKEN
+        const headers = {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28"
+        }
+        axios.get('https://api.notion.com/v1/databases/9344c853e4cd4a7b9db8d3a8c7e6bcae?v=73bf70c277a647fdafb5f052256308b6', { headers: headers })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.error(error)
+        })
     }
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
+// import axios from 'axios'
 import './index.css'
 
 function App() {
   const [url, setUrl] = useState("nothing")
+  const [comment, setComment] = useState("")
   const getUrl = () => {
     chrome.runtime.sendMessage({ action: "getLastAccessedUrl"}, response => {
       if (response.url) {
@@ -14,9 +16,16 @@ function App() {
       }
     })
   }
+  const addInNotion = () => {
+    chrome.runtime.sendMessage({ action: "addInNotion", comment: comment}, response => {
+      console.log(response)
+    })
+  }
   return (
     <>
       <button onClick={getUrl}>Get URL{url}</button>
+      <input value={comment} onChange={e => setComment(e.target.value)} />
+      <button onClick={addInNotion}>Add in Notion</button>
     </>
   )
 }
